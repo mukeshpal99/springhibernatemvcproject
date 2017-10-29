73de539f6cf14763395ab1a38a98acb0056a6a57
mukeshpalad <mukesh.pal@appdirect.com>
2017-10-29 15:00:55 +0530

    first commit

---
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+				List of Customers	
+		</title>
+	</head>
+
+	<body>
+		List Customer Place Holder-- Coming soon.....
+	</body>
+
+</html>
---
+++ src/mvc/com/learning/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package mvc.com.learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model theModel){
+		
+		return "list-customers";
+	}
+
+}
